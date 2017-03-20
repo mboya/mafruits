4960cc22d30b8d953991d0d8b8be04de4811c367
mboya <mboyaberry@gmail.com>
2017-03-20 16:27:33 +0300

    entry point for the api and route

---
+++ app/controllers/access_controller.rb
@@ -0,0 +1,5 @@
+class AccessController < ApplicationController
+  def updates
+    render text: 'ok'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   # messenger
   match 'messenger/updates', to: 'messenger#verify', as: 'handle_messenger_verification', via: 'get'
   match 'messenger/updates', to: 'messenger#updates', as: 'handle_messenger_updates', via: 'post'
+
+  match 'api/updates', to: 'access#updates', as: 'handle_api_updates', via: 'post'
 end
